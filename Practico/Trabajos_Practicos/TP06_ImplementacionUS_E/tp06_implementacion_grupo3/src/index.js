3f9ac9b06cf1cdfd4d968e080c3dd22b318f0d71
Federico Host <federico.host@gmail.com>
2022-09-02 16:49:29 -0300

    Inicio de proyecto React

--- Practico/Trabajos_Practicos/TP06_ImplementacionUS_E/tp06_implementacion_grupo3/src/index.js
+++ Practico/Trabajos_Practicos/TP06_ImplementacionUS_E/tp06_implementacion_grupo3/src/index.js
@@ -1,15 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./components/layout";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Delivereat!</h1>} />
+        <Route path="/" element={<Layout><h1>Delivereat!</h1></Layout>} />
         <Route path="/checkout" element={<h1>Buy</h1>} />
       </Routes>
     </BrowserRouter>
