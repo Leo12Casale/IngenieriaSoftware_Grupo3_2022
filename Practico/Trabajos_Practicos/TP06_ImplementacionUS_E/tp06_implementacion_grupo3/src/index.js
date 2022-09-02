da5b6251808c009b37e92d53705c4d325fb662ff
ValentinTT <valentintapiatorti@gmail.com>
2022-09-02 16:14:36 -0300

    Instalación de react-router v6

--- Practico/Trabajos_Practicos/TP06_ImplementacionUS_E/tp06_implementacion_grupo3/src/index.js
+++ Practico/Trabajos_Practicos/TP06_ImplementacionUS_E/tp06_implementacion_grupo3/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <button className='btn'>
-      Hola
-    </button>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Delivereat!</h1>} />
+        <Route path="/checkout" element={<h1>Buy</h1>} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
