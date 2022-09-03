d31a47d57f9c10a0e2ce993ef6e46a500e391ccd
ValentinTT <valentintapiatorti@gmail.com>
2022-09-03 09:18:03 -0300

    Corrección imagen responsive de paquete
    Se agregó un div más para poder solucionar el problema de ancho de la imagen del componente

--- Practico/Trabajos_Practicos/TP06_ImplementacionUS_E/tp06_implementacion_grupo3/src/routes/order.js
+++ Practico/Trabajos_Practicos/TP06_ImplementacionUS_E/tp06_implementacion_grupo3/src/routes/order.js
@@ -4,19 +4,28 @@ import PropTypes from "prop-types";
 function Paquete({ imgUrl, title, description, quantity, amount }) {
   return (
     <div class="card card-side bg-base-100 shadow-xl">
-      <figure class="rounded-md overflow-hidden h-fit w-fit self-center ml-2">
+      {/* <figure class="rounded-l-md overflow-hidden h-full w-72 self-center">
         <img src={imgUrl} alt="Movie" />
-      </figure>
-      <div class="card-body p-4">
-        <h2 class="card-title">{title}</h2>
-        <p>{description}</p>
-        <div class="card-actions flex items-center">
-          <div class="text-white font-semibold rounded-md w-fit">
-            x{quantity}
-          </div>
-          <span class="grow"></span>
-          <div class="bg-primary stat text-white rounded-md w-fit">
-            ${amount} c/u
+      </figure> */}
+      <div class="sm:flex">
+        <div class="sm:shrink-0">
+          <img
+            class="h-48 w-full object-cover sm:h-full sm:w-48"
+            src={imgUrl}
+            alt="Movie"
+          />
+        </div>
+        <div class="card-body p-4">
+          <h2 class="card-title">{title}</h2>
+          <p>{description}</p>
+          <div class="card-actions flex items-center">
+            <div class="text-white font-semibold rounded-md w-fit">
+              x{quantity}
+            </div>
+            <span class="grow"></span>
+            <div class="bg-primary stat text-white rounded-md w-fit">
+              ${amount} c/u
+            </div>
           </div>
         </div>
       </div>
@@ -33,7 +42,7 @@ Paquete.propTypes = {
 };
 
 Paquete.defaultProps = {
-  imgUrl: "https://placeimg.com/200/200/arch",
+  imgUrl: "https://placeimg.com/500/500/arch",
   title: "Hamburguesa de Messi",
   description:
     "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, id sequi nostrum harum perspiciatis at necessitatibus expedita",
@@ -43,7 +52,7 @@ Paquete.defaultProps = {
 
 const mockData = [
   {
-    imgUrl: "https://placeimg.com/200/200/arch",
+    imgUrl: "https://placeimg.com/500/500/arch",
     title: "Hamburguesa de Messi",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, id sequi nostrum harum perspiciatis at necessitatibus expedita",
@@ -51,7 +60,7 @@ const mockData = [
     amount: 1700,
   },
   {
-    imgUrl: "https://placeimg.com/200/200/arch",
+    imgUrl: "https://placeimg.com/500/500/arch",
     title: "Hamburguesa de Messi",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, id sequi nostrum harum perspiciatis at necessitatibus expedita",
@@ -59,7 +68,7 @@ const mockData = [
     amount: 1700,
   },
   {
-    imgUrl: "https://placeimg.com/200/200/arch",
+    imgUrl: "https://placeimg.com/500/500/arch",
     title: "Hamburguesa de Messi",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, id sequi nostrum harum perspiciatis at necessitatibus expedita",
@@ -67,7 +76,7 @@ const mockData = [
     amount: 1700,
   },
   {
-    imgUrl: "https://placeimg.com/200/200/arch",
+    imgUrl: "https://placeimg.com/500/500/arch",
     title: "Hamburguesa de Messi",
     description:
       "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Deleniti, id sequi nostrum harum perspiciatis at necessitatibus expedita",
@@ -95,11 +104,11 @@ export default function Order() {
             key={`idx-${idx}`}
           />
         ))}
-        <div class="sticky bottom-0 left-0 right-0 font-medium text-xl text-neutral-focus flex justify-end items-center space-x-3 p-4">
-          <div class="bg-neutral-content p-3 rounded-md shadow-xl bg-opacity-70">
+        <div class="sticky bottom-0 left-0 right-0 font-medium text-xl text-neutral-focus flex flex-col sm:flex-row items-end justify-end space-y-3 sm:items-center sm:space-x-3 sm:space-y-0 p-4">
+          <div class="bg-neutral-content p-3 rounded-md shadow-xl bg-opacity-70 w-fit">
             <p>Monto final: ${monto}</p>
           </div>
-          <button class="btn bg-accent text-white p-3 rounded-md shadow-xl border-0">
+          <button class="btn bg-accent text-white p-3 rounded-md shadow-xl border-0 w-fit">
             Comprar
           </button>
         </div>
